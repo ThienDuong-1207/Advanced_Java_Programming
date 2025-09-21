1cc3630c52e349210f40749511cb795738be4dc4
ThienDuong-1207 <dctthien1201@gmail.com>
2025-09-21 17:02:47 +0700

    Third

---
+++ Lab2_AdvJava/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Spring Boot!";
+    }
+
+    @GetMapping("/info")
+    public Map<String, String> getInfo() {
+        return Map.of(
+            "course", "Advanced Java Programming",
+            "class", "251_72ITDS30103_01",
+            "student", "Duong Chi Thien"
+        );
+    }
+}
